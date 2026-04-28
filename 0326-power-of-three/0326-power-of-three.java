bf3726c0e02e0ac7a73a72b1137f89576a920f9c
Ritika Yadav <ritikayadav675@gmail.com>
2026-04-28 20:16:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,5 +1,15 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        
+        //base case
+        if (n == 0) return false;
+        while(n != 1){
+            int rem = n % 3;
+            if (rem != 0){
+                return false;
+            }
+            n = n/3;
+        }
+        return true;
     }
 }
+
