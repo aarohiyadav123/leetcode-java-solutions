8b50ac6be56b2712c1d1038ef3c2bed91c93d656
Ritika Yadav <ritikayadav675@gmail.com>
2026-05-08 20:14:41 +0530

    Time: 3 ms (43.45%), Space: 46.5 MB (97.6%) - LeetHub

---
+++ 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int countParity(int x, int[]nums, int i){
+    int count =0;
+    int n = nums.length;
+    if(x % 2 == 0){
+        for(int j = i+1; j<n; j++){
+            if(nums[j] % 2 != 0){
+                count++;
+            }
+        }
+    }
+    else{
+        for(int j = i+1; j<n; j++){
+            if(nums[j] % 2 ==0){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+    public int[] countOppositeParity(int[] nums) {
+        int n = nums.length;
+        int[]  res = new int[n]; 
+        for(int i =0; i<n; i++){
+            res[i]= countParity(nums[i] , nums, i);
+
+            }
+        
+      return res;  
+    }
+}
