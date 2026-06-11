f7fb2a59bd2baae89e433e2c4979f2d2e5a1f4e8
Ritika Yadav <ritikayadav675@gmail.com>
2026-06-12 00:59:52 +0530

    Time: 0 ms (100%), Space: 42.1 MB (93.99%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,18 +1,16 @@
 class Solution {
-    public boolean isHappy(int n) { 
-       Set<Integer> set = new HashSet<>();
-       while(n != 1){ 
-         if(set.contains(n) == true) return false;
-         set.add(n);
-         int sum = 0;
-         while(n != 0){
+    public boolean isHappy(int n) {
+       if( n ==1 || n == 7) return  true;
+       if(n < 10 ) return false;
+       else{
+        int sum = 0;
+        while( n != 0){
             int rem = n % 10;
-            sum = sum + rem*rem;
-            n = n/ 10;
-         }
-         n = sum;
+            sum = sum + rem * rem;
+            n = n/10;
+        }
+        return  isHappy(sum);
        }
-       return true;
     }
+
 }
-// ye hashset se kiya huaa question hai 
