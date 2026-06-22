da342c8f91c62f31a96c5ab7d15b301a0c4c2ec8
Ritika Yadav <ritikayadav675@gmail.com>
2026-06-23 00:36:15 +0530

    Time: 10 ms (24.89%), Space: 42.1 MB (51.31%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,21 +1,11 @@
-
-class Solution {
-    int fib(int n) {
-        if(n == 0 || n == 1){ 
-        return n ;
-    }
-    int a = 0;
-    int b = 1;
-    int sum = 0;
-    for(int i = 2; i<=n; i=i+1){
-        sum = a + b;
-        a = b;
-        b = sum;
-    }
-    return sum;
-
+class  Solution {
+    public int fib(int n){
+        if(n ==1) return 1;
+        if(n == 0) return 0;
+        int left = fib(n-1);
+        int right = fib(n-2);
+        return left+right;
         
     }
-};
-
-//ye normal hai jo basic hai febbonachi ka 
+}
+//fib ko recurion se slove kiya hai 
