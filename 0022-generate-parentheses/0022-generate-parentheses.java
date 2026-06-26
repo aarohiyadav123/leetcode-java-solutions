09abc53388093a929e70cff2dec8b52e986014cc
Ritika Yadav <ritikayadav675@gmail.com>
2026-06-26 20:23:20 +0530

    Time: 2 ms (71.93%), Space: 44.9 MB (22.6%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,15 +1,12 @@
 class Solution {
 public void helper(int n, String curr, int lc, int rc, List<String> ans){
-        //Base case
         if(curr.length() == 2*n){
            ans.add(curr);
            return;
        }
-        //pick left 
         if(lc < n){
             helper(n, curr+"(", lc+1, rc, ans);
         }
-        //pick right
         if(rc < lc){
            helper(n, curr+")", lc, rc+1, ans);
         }
