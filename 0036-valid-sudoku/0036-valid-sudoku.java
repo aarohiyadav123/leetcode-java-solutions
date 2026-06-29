9b061d4c687471048f62894b15f19989d34eece3
Ritika Yadav <ritikayadav675@gmail.com>
2026-06-29 20:14:28 +0530

    Time: 1 ms (99.96%), Space: 46.5 MB (59.17%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,48 +1,34 @@
 class Solution {
-
-    public boolean isValid(char[][] board, int rows, int cols, char ch) {
-
-        // Row 
-        for (int j = 0; j < 9; j++) {
-            if (board[rows][j] == ch) return false;
+    public boolean isValid(char[][] board, int row, int col, char ch){
+        //check horizontally
+        for(int j = 0; j<9; j++){
+            if(board[row][j] == ch) return false;
         }
-
-        // Column 
-        for (int i = 0; i < 9; i++) {
-            if (board[i][cols] == ch) return false;
+        //check vertically
+        for(int i = 0; i<9; i++){
+            if(board[i][col] == ch) return false;
         }
-
-        
-        int rGrid = (rows / 3) * 3;
-        int cGrid = (cols / 3) * 3;
-
-        for (int i = rGrid; i < rGrid + 3; i++) {
-            for (int j = cGrid; j < cGrid + 3; j++) {
-                if (board[i][j] == ch) return false;
+        //now for 3X3 grid
+        int rGrid = row/3*3;
+        int cGrid = col/3*3;
+        for(int i = rGrid; i<rGrid+3; i++){
+            for(int j = cGrid; j<cGrid+3; j++){
+                if(board[i][j] == ch) return false;
             }
         }
-
         return true;
     }
-
     public boolean isValidSudoku(char[][] board) {
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-
-                if (board[i][j] == '.') continue;
-
-                char ch = board[i][j];
-                board[i][j] = '.';
-
-                if (!isValid(board, i, j, ch)) {
-                    return false;
+        for(int i = 0; i<9; i++){
+            for(int j = 0; j<9; j++){
+                if(board[i][j] != '.'){
+                    char ch = board[i][j]; //'5'
+                    board[i][j] = '.'; //delete
+                    if(isValid(board, i , j, ch) == false) return false;
+                    board[i][j] = ch; //reset
                 }
-
-                board[i][j] = ch;
             }
         }
-
         return true;
     }
 }
